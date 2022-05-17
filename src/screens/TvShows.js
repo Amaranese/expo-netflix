46f8b9c81a38e3754bc1f09dd45a9f96fb937f40
Amaranese <alejandromaranes@gmail.com>
2022-05-17 10:58:21 +0200

    Create TvShows.js

---
+++ src/screens/TvShows.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { gStyle } from '../constants';
+
+// components
+import Cast from '../components/Cast';
+import HeaderHome from '../components/HeaderHome';
+
+const TvShows = () => (
+  <View style={gStyle.container}>
+    <HeaderHome show />
+
+    <View style={gStyle.spacer12} />
+
+    <View style={gStyle.pHHalf}>
+      <Text style={gStyle.heading}>TV Shows</Text>
+    </View>
+
+    <Cast />
+  </View>
+);
+
+export default TvShows;
