cd758d158d505a1d2269f6bfeed927394e1514df
Amaranese <alejandromaranes@gmail.com>
2022-05-17 10:59:50 +0200

    .

---
+++ src/screens/Movies.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { gStyle } from '../constants';
+
+// components
+import Cast from '../components/Cast';
+import HeaderHome from '../components/HeaderHome';
+
+const Movies = () => (
+  <View style={gStyle.container}>
+    <HeaderHome show />
+
+    <View style={gStyle.spacer12} />
+
+    <View style={gStyle.pHHalf}>
+      <Text style={gStyle.heading}>Movies</Text>
+    </View>
+
+    <Cast />
+  </View>
+);
+
+export default Movies;
