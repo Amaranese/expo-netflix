ce4c7e843e77ca1771dcf8b3c2c360806ffcbb31
Amaranese <alejandromaranes@gmail.com>
2022-05-17 10:58:25 +0200

    Create Search.js

---
+++ src/screens/Search.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
+import { gStyle } from '../constants';
+
+// components
+import Cast from '../components/Cast';
+import HeaderSearch from '../components/HeaderSearch';
+
+const Search = () => (
+  <React.Fragment>
+    <TouchableWithoutFeedback accessible={false} onPress={Keyboard.dismiss}>
+      <View style={gStyle.container}>
+        <HeaderSearch />
+      </View>
+    </TouchableWithoutFeedback>
+
+    <Cast />
+  </React.Fragment>
+);
+
+export default Search;
