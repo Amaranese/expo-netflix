f2f3cf13e0a1c821fce1d2216a5a012d518d73c5
Amaranese <alejandromaranes@gmail.com>
2022-05-17 10:58:50 +0200

    Create MyList.js

---
+++ src/screens/MyList.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { gStyle } from '../constants';
+
+// components
+import Cast from '../components/Cast';
+import HeaderHome from '../components/HeaderHome';
+
+const MyList = () => (
+  <View style={gStyle.container}>
+    <HeaderHome show />
+
+    <View style={gStyle.spacer12} />
+
+    <View style={gStyle.pHHalf}>
+      <Text style={gStyle.heading}>My List</Text>
+    </View>
+
+    <Cast />
+  </View>
+);
+
+export default MyList;
